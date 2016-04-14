8dba87bc9cfb4618751aba03fb89c9bdfbffd1fa
Michael Ketiku <mketiku@gmail.com>
2016-04-14 12:29:14 -0400

    moooo

---
+++ lpthw/ex10.py
@@ -0,0 +1,7 @@
+# #escape characters in python include 
+# ''' ''' , \ , 
+
+
+tabby_cat = "\tI am tabbed in" 
+persian_cat = "\t I am split\ non a line"
+backslash_cat = "I am\\ a \\ cat"
---
+++ lpthw/ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#print items on new lines,
+print ("Days of the week",days)
+print ("Months of the year",months)
+
+print (""" 
+There is something going on here.
+With the three double-quotes. 
+We will be able to type as much as we like. 
+Even 4 lines if we want, or 5 or 6.
+""")
