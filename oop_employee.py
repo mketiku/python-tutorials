483b6a76238b633470e723ca7aa83828c1c215fd
Michael Ketiku <mketiku@gmail.com>
2016-10-14 16:00:47 -0400

    learned about instances vs class variables

--- oop_employee.py
+++ oop_employee.py
@@ -1,8 +1,11 @@
 # Python Object-Oriented Programming
 
+
 class Employee:
     '''Simple class to hold Employee information at a company'''
 
+    num_of_emps = 0
+    raise_amount = 1.04 # class variable
     
     def __init__(self,first,last,pay):
         # init method with attributes of an employee specified
@@ -13,17 +16,26 @@ class Employee:
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)# example 
         
 emp_1 = Employee('Test','User',9999)
 emp_2 = Employee('Michael','Ketiku', 10000)
 
+print(Employee.__dict__)
+
+emp_1.raise_amount =  1.05
+
+print(emp_1.__dict__)
 # Instance variables that is unique to each isinstance
 
-print(emp_1.email)
-print(emp_2.email)
+# print(emp_1.email)
+# print(emp_2.email)
 
-# print(emp_1.fullname()) # parantheses allows us to use the actual method instead of the name.
-# print(emp_2.fullname())
+# # print(emp_1.fullname()) # parantheses allows us to use the actual method instead of the name.
+# # print(emp_2.fullname())
 
-print(emp_1.fullname()) # instance employee 1 
-print(Employee.fullname(emp_1))
+# print(emp_1.pay)
+# emp_1.apply_raise()
+# print(emp_1.pay)
