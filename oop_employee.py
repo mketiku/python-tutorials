dc4baf86211d2f0f24895455047e772a49899f69
Michael Ketiku <mketiku@gmail.com>
2016-10-14 15:44:35 -0400

    add example instance variables

--- oop_employee.py
+++ oop_employee.py
@@ -1,27 +1,29 @@
 # Python Object-Oriented Programming
 
 class Employee:
+    '''Simple class to hold Employee information at a company'''
 
+    
+    def __init__(self,first,last,pay):
+        # init method with attributes of an employee specified
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
 
-emp_1 = Employee()
-emp_2 = Employee()
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+        
+emp_1 = Employee('Test','User',9999)
+emp_2 = Employee('Michael','Ketiku', 10000)
 
 # Instance variables that is unique to each isinstance
 
-print(emp_1) 
-print(emp_2)
-
-
-emp_1.first = 'Corey'
-emp_1.last = 'Schafer'
-emp_1.email = 'Corey.Schafer@company.com'
-emp_1.pay = 5000
-
-emp_2.first = 'Michael'
-emp_2.last = 'Ketiku'
-emp_2.email = 'mketiku@gmail.com'
-emp_2.pay = 5000
-
-
 print(emp_1.email)
 print(emp_2.email)
+
+# print(emp_1.fullname()) # parantheses allows us to use the actual method instead of the name.
+# print(emp_2.fullname())
+
+print(emp_1.fullname()) # instance employee 1 
+print(Employee.fullname(emp_1))
