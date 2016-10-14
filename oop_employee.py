93e7a717c63b48f059a5894fe5bf14bb29f37e06
Michael Ketiku <mketiku@gmail.com>
2016-10-14 16:18:13 -0400

    learned the difference between instance, class and static methods

--- oop_employee.py
+++ oop_employee.py
@@ -1,5 +1,5 @@
 # Python Object-Oriented Programming
-
+import datetime
 
 class Employee:
     '''Simple class to hold Employee information at a company'''
@@ -18,24 +18,51 @@ class Employee:
         return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amount)# example 
+         self.pay = int(self.pay * self.raise_amount)# example 
+    
+    @classmethod
+    def set_raise_amt(cls,amount):
+        '''
+        Regular instance method
+        A class method that allows us to set the raise amount
+        '''
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls,emp_str):
+        '''
+        Alternative constructor
+        Construct an employee by parsing a string 
+        '''
+        first,last, pay = emp_str.split('-')
+        return cls(first,last,pay)
+
+    @staticmethod
+    def  is_workday(day):
+        '''
+        Simple function that take a date and returns whether or not
+        it was a work day
+        '''
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
         
 emp_1 = Employee('Test','User',9999)
 emp_2 = Employee('Michael','Ketiku', 10000)
 
-print(Employee.__dict__)
 
-emp_1.raise_amount =  1.05
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
 
-print(emp_1.__dict__)
-# Instance variables that is unique to each isinstance
+new_emp_1 = Employee.from_string(emp_str_1)
 
-# print(emp_1.email)
-# print(emp_2.email)
+print(new_emp_1.email)
+print(new_emp_1.pay)
 
-# # print(emp_1.fullname()) # parantheses allows us to use the actual method instead of the name.
-# # print(emp_2.fullname())
+my_date = datetime.date(2016,10,6)
 
-# print(emp_1.pay)
-# emp_1.apply_raise()
-# print(emp_1.pay)
+print(Employee.is_workday(my_date))# Employee.set_raise_amt(1.05)
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
