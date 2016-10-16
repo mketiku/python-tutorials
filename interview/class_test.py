2d5dd1bc33a57e13e0cef8cb4be9c38a10ec3a01
Michael Ketiku <mketiku@gmail.com>
2016-10-16 18:10:05 -0400

    learned about classes and kwargs

---
+++ interview/class_test.py
@@ -0,0 +1,117 @@
+class A(object):
+    def go(self):
+        print("go A go!")
+    def stop(self):
+        print("stop A stop!")
+    def pause(self):
+        raise Exception("Not Implemented")
+
+class B(A):
+    def go(self):
+        super(B, self).go()
+        print("go B go!")
+
+class C(A):
+    def go(self):
+        super(C, self).go()
+        print("go C go!")
+    def stop(self):
+        super(C, self).stop()
+        print("stop C stop!")
+
+class D(B,C):
+    def go(self):
+        super(D, self).go()
+        print("go D go!")
+    def stop(self):
+        super(D, self).stop()
+        print("stop D stop!")
+    def pause(self):
+        print("wait D wait!")
+
+class E(B,C): pass
+
+a = A()
+b = B()
+c = C()
+d = D()
+e = E()
+
+# specify output from here onwards
+
+a.go()
+b.go()
+c.go()
+d.go()
+e.go()
+
+a.stop()
+b.stop()
+c.stop()
+d.stop()
+e.stop()
+
+a.pause()
+b.pause()
+c.pause()
+d.pause()
+e.pause()
+
+'''
+Solutions
+a.go()
+# go A go!
+
+b.go()
+# go A go!
+# go B go!
+
+c.go()
+# go A go!
+# go C go!
+
+d.go()
+# go A go!
+# go C go!
+# go B go!
+# go D go!
+
+e.go()
+# go A go!
+# go C go!
+# go B go!
+
+a.stop()
+# stop A stop!
+
+b.stop()
+# stop A stop!
+
+c.stop()
+# stop A stop!
+# stop C stop!
+
+d.stop()
+# stop A stop!
+# stop C stop!
+# stop D stop!
+
+e.stop()
+# stop A stop!
+
+a.pause()
+# ... Exception: Not Implemented
+
+b.pause()
+# ... Exception: Not Implemented
+
+c.pause()
+# ... Exception: Not Implemented
+
+d.pause()
+# wait D wait!
+
+e.pause()
+# ...Exception: Not Implemented
+
+'''
